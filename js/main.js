001c52b0d98eb28a1703491997075e14a28e546e
faxont <120399762+faxont@users.noreply.github.com>
2024-09-19 12:44:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ document.body.appendChild(canvas);
 canvas.width = window.innerWidth*.98;
 canvas.height = window.innerHeight*.98;
 
+let characters = [];
+const renderer=new Renderer();
+
+characters[0] = new Character('Bill',11,10,10,8);
+characters[0].calculateSecondaryAttributes();
+characters[0].selectWeaponByName("Sword");
+
+characters[1] = new Character('Bob',10,11,10,8);
+characters[1].calculateSecondaryAttributes();
+characters[1].selectWeaponByName("Spear");
+
+renderer.render(characters);
